52f1fe6fbb04eda076284f068f7a5b768fe12a13
medhakg205 <medhakg12@gmail.com>
2026-03-04 23:27:52 +0530

    UpdatedAuth.js

--- routes/auth.js
+++ routes/auth.js
@@ -11,16 +11,16 @@ router.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // Check if user exists
+    
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "Email already registered" });
     }
 
-    // Hash password
+    
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create user
+    
     const user = await User.create({
       name,
       email,
@@ -35,7 +35,7 @@ router.post("/register", async (req, res) => {
   console.error("REGISTER ERROR:", error);
   res.status(500).json({
     message: "Server error",
-    error: error.message   // 👈 send actual message
+    error: error.message  
   });
 }
 });
@@ -46,19 +46,19 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check user
+    
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    // Compare password
+    
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
 
-    // Generate token
+    
     const token = jwt.sign(
       { userId: user._id },
       process.env.JWT_SECRET,
